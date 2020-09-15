5227f512d382fa7784246840eee55f95d2993e57
ashukumar2001 <kumar.ashu414283@gmail.com>
2020-09-16 01:51:39 +0530

    dark-mode added

--- dark_mode.js
+++ dark_mode.js
@@ -1,14 +1,10 @@
-let darkModeBtn = document.querySelector("#toggle-dark-mode");
-darkModeBtn.addEventListener('click', () => {
-    let body = document.body;
-    let restartBtn = document.querySelector(".game--restart");
+let toggleDarMode = document.querySelector(".theme-switch input");
+toggleDarMode.addEventListener('click', () => {
     let cells = document.querySelectorAll(".cell");
-    darkModeBtn.classList.toggle("dark-mode");
-    body.classList.toggle("dark-mode");
-    restartBtn.classList.toggle("dark-mode");
     cells.forEach(cell => {
-        cell.classList.toggle("dark-mode");
+        cell.classList.toggle("dark-mode")
     })
-    let toggleIcon = document.querySelector("i#toggle-icon")
-
+    document.body.classList.toggle("dark-mode");
+    let restartButton = document.querySelector(".game--restart");
+    restartButton.classList.toggle("dark-mode");
 })
